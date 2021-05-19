3980ea72b5473807115fc28a1c8cae5c2cf474a4
Mariia Borysova <>
2021-05-19 18:18:48 +0200

    Initial commit

---
+++ form/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path, include
+
+from . import views
+
+app_name = 'form'
+
+urlpatterns = [
+    # ex: /
+    path('', views.home, name='home'),
+    path('simulation/', views.simulation_view, name='simulation'),
+]
---
+++ form/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
+
+#from .models import Leads
+
+
+def home(request):
+    test = {
+        "var": False
+    }
+    return render(request, 'index.html', test)
+
+def simulation_view(request):
+    return render(request, 'simulation.html')
+
+"""
+def signup(request):
+    leads = Leads()
+    status = leads.insert_lead(request.POST['name'], request.POST['email'], request.POST['previewAccess'])
+    return HttpResponse('', status=status)
+"""
