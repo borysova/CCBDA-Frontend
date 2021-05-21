888ef65c69f4063527f9b054de42cd941c9408ce
Mariia Borysova <>
2021-05-21 14:30:41 +0200

    parameters submission form

--- form/views.py
+++ form/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponseRedirect
 
 def home(request):
     test = {
-        "var": False
+        "var": True
     }
     return render(request, 'index.html', test)
 
@@ -21,3 +21,26 @@ def signup(request):
     status = leads.insert_lead(request.POST['name'], request.POST['email'], request.POST['previewAccess'])
     return HttpResponse('', status=status)
 """
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import NameForm
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'simulation.html', {'form': form})
