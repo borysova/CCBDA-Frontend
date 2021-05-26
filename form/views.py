c1313fbb138c9fbb47e1d5a0bdc2eee52cc3bb0f
Mariia Borysova <>
2021-05-26 16:40:08 +0200

    design updated

--- form/views.py
+++ form/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import NameForm
-#from .models import Leads
 
 simulationRunning = False
 
@@ -23,10 +21,3 @@ def home(request):
 
 def simulation_view(request):
     return render(request, 'simulation.html')
-
-"""
-def signup(request):
-    leads = Leads()
-    status = leads.insert_lead(request.POST['name'], request.POST['email'], request.POST['previewAccess'])
-    return HttpResponse('', status=status)
-"""
