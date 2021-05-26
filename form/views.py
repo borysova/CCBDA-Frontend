78bc50d9b3e02685c3b8f109fa5ab76523a40916
Mariia Borysova <>
2021-05-26 02:23:58 +0200

    design updated

--- form/views.py
+++ form/views.py
@@ -6,10 +6,18 @@ from django.shortcuts import render
 from .forms import NameForm
 #from .models import Leads
 
+simulationRunning = False
+
+def setSimState(request):
+    global simulationRunning
+    if request.POST['text'] == "True":
+        simulationRunning = True
+    else:
+        simulationRunning = False
 
 def home(request):
     test = {
-        "var": True
+        "simRunning": simulationRunning
     }
     return render(request, 'index.html', test)
 
@@ -22,21 +30,3 @@ def signup(request):
     status = leads.insert_lead(request.POST['name'], request.POST['email'], request.POST['previewAccess'])
     return HttpResponse('', status=status)
 """
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'simulation.html', {'form': form})
