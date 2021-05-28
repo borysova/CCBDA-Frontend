025bf514e38ba8c1540a51ba305a7b82893aec6f
francescroy <francescroy@gmail.com>
2021-05-28 11:01:27 +0200

    almost finished

--- form/views.py
+++ form/views.py
@@ -20,4 +20,26 @@ def home(request):
     return render(request, 'index.html', test)
 
 def simulation_view(request):
-    return render(request, 'simulation.html')
+
+    individuals = request.POST['individuals']
+    incubation = request.POST['incubation']
+    symptomatic = request.POST['symptomatic']
+    immunity = request.POST['immunity']
+    mortality = request.POST['mortality']
+    scale_city = request.POST['scale_city']
+    max_rounds = request.POST['max_rounds']
+    id_simulation = request.POST['id_simulation']
+
+    parameters = {
+        "individuals": individuals,
+        "incubation": incubation,
+        "symptomatic": symptomatic,
+        "immunity": immunity,
+        "mortality": mortality,
+        "scale_city": scale_city,
+        "max_rounds": max_rounds,
+        "id_simulation": id_simulation,
+    }
+
+
+    return render(request, 'simulation.html',parameters)
