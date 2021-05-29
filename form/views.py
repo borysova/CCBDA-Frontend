87d24a45c41342626a46159709b43584f904db40
francescroy <francescroy@gmail.com>
2021-05-29 13:16:09 +0200

    almost_final

--- form/views.py
+++ form/views.py
@@ -1,34 +1,49 @@
 from django.shortcuts import render
 
-
 def home(request):
     return render(request, 'index.html')
 
 def simulation_view(request):
-    individuals = request.POST['individuals']
-    incubation = request.POST['incubation']
-    symptomatic = request.POST['symptomatic']
-    immunity = request.POST['immunity']
-    mortality = request.POST['mortality']
-    scale_city = request.POST['scale_city']
-    max_rounds = request.POST['max_rounds']
-    id_simulation = request.POST['id_simulation']
-    beacons_list = request.POST.getlist('beacons')
+    try:
+        individuals = request.POST['individuals']
+        incubation = request.POST['incubation']
+        symptomatic = request.POST['symptomatic']
+        immunity = request.POST['immunity']
+        mortality = request.POST['mortality']
+        scale_city = request.POST['scale_city']
+        max_rounds = request.POST['max_rounds']
+        id_simulation = request.POST['id_simulation']
+        beacons_list = request.POST.getlist('beacons')
 
-    beacons = "No"
-    if len(beacons_list) != 0:
-        beacons = "Yes"
+        beacons = "No"
+        if len(beacons_list) != 0:
+            beacons = "Yes"
 
-    parameters = {
-        "individuals": individuals,
-        "incubation": incubation,
-        "symptomatic": symptomatic,
-        "immunity": immunity,
-        "mortality": mortality,
-        "scale_city": scale_city,
-        "max_rounds": max_rounds,
-        "id_simulation": id_simulation,
-        "beacons": beacons,
-    }
+        parameters = {
+            "individuals": individuals,
+            "incubation": incubation,
+            "symptomatic": symptomatic,
+            "immunity": immunity,
+            "mortality": mortality,
+            "scale_city": scale_city,
+            "max_rounds": max_rounds,
+            "id_simulation": id_simulation,
+            "beacons": beacons,
+        }
 
-    return render(request, 'simulation.html', parameters)
+        return render(request, 'simulation.html', parameters)
+
+    except:
+        parameters = {
+            "individuals": "",
+            "incubation": "",
+            "symptomatic": "",
+            "immunity": "",
+            "mortality": "",
+            "scale_city": "",
+            "max_rounds": "",
+            "id_simulation": "",
+            "beacons": "",
+        }
+
+        return render(request, 'simulation.html', parameters)
